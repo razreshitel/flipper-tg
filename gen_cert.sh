1585cbe36f50475f00422542a9669573433a8eee
razreshitel <sashok2312@gmail.com>
2026-05-26 01:55:34 +0300

    Fix path in gen_cert.sh: flipper-telegram → flipper-tg

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo openssl req -x509 -newkey rsa:2048 -keyout /home/pi/flipper-telegram/key.pem -out /home/pi/flipper-telegram/cert.pem -days 3650 -nodes -subj "/CN=192.168.0.102"
+sudo openssl req -x509 -newkey rsa:2048 -keyout /home/pi/flipper-tg/key.pem -out /home/pi/flipper-tg/cert.pem -days 3650 -nodes -subj "/CN=192.168.0.102"
